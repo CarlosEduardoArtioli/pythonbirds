ded8311e30ed392590246bd6abbd0232f0292e1f
Carlos Eduardo Artioli <caduartioli@gmail.com>
2021-01-25 19:20:01 -0300

    Criados atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,12 @@
 class Pessoa:
-    pass
+    def __init__(self, nome=None, idade=18):
+        self.nome = nome
+        self.idade = idade
+
+    def cumprimentar(self):
+        return f'Olá {self.nome}'
+
+
+if __name__ == '__main__':
+    p = Pessoa('Cadu')
+    print(p.cumprimentar())
